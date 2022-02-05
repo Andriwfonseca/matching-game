fcc0d5e7cc71bbb85dca807c95657169bcf3488c
Andriwfonseca <63034338+Andriwfonseca@users.noreply.github.com>
2022-02-05 18:00:06 -0300

    Create ScoreBoard object

---
+++ src/objects/ScoreBoard/index.js
@@ -0,0 +1,13 @@
+import PlayerName from "../../components/PlayerName";
+
+
+const ScoreBoard = () =>{
+    return /*html*/`
+        <header class="score-board">
+            ${PlayerName("Player 1")}
+            ${PlayerName("Player 2")}
+        </header>
+    `;
+}
+
+export default ScoreBoard;
