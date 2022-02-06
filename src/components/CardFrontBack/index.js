71f39ad1154a4e1f4c0314545ecda25921d98648
Andriwfonseca <63034338+Andriwfonseca@users.noreply.github.com>
2022-02-06 20:39:20 -0300

    Add click event on CardFrontBack component

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,9 +1,26 @@
 import CardGame from "../CardGame";
 import "./style.css";
 
-const CardFrontBack = () =>{
+
+
+const CardFrontBack = () =>{    
+    window.cardFrontBack = {}
+    window.cardFrontBack.handleClick = (event) =>{
+        const $origin = event.target;
+        const $cardFrontBack = $origin.closest('.card-front-back');
+
+        /*
+        if($cardFrontBack.classList.contains('-active')){
+            $cardFrontBack.classList.remove('-active');
+        }else{
+            $cardFrontBack.classList.add('-active');
+        }*/
+        /*toggle faz o mesmo que esse código acima*/
+        $cardFrontBack.classList.toggle('-active');
+    }
+
     return /*html*/`
-        <article class="card-front-back">
+        <article class="card-front-back" onclick="cardFrontBack.handleClick(event)">
             <div class="card -front">
                 ${CardGame()}
             </div>    
